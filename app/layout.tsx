4302d97fc768e449bd6ea861318b60d45b9e7f54
kgs global <usama3245078@gmail.com>
2026-06-09 00:57:11 +0500

    fix all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from 'next';
-import '../styles/globals.css';
-
+import type { Metadata } from 'next';';
+import {  } from "module";
 export const metadata: Metadata = {
   title: 'DPP Compliance Platform',
   description: 'Digital Product Passport - ESPR Compliance',
